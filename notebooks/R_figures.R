88bfe0ea73f5ca50517baee410f6fd540c465ee0
Denis Shepelin <denshe@biosustain.dtu.dk>
2019-05-16 15:12:09 +0200

    Utility script to generate ggplot graphics for anova and knockout simulations

---
+++ notebooks/R_figures.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+library(plotly)
+
+# Plot for errors per model
+p <- read_csv("../data/processed/normalized_errors.csv") %>%
+  select(-X1) %>%
+  filter(author != "Ishii" & author != "Ec_core") %>%
+  mutate(author = factor(author)) %>%
+  mutate(author = fct_relevel(author, levels=c("Khodayari", "Kurata", "Millard", "iML1515", "ECC2","Exp_ECC2", "Exp_iML1515"))) %>%
+  ggplot(aes(x=author, y=normalized_error, color=author, label=sample_id)) 
+
+errors_p <- p + geom_point() + geom_jitter(width=0.3) + ggtitle("Normalized error knockout simulations")
+# It's possible to save it as html using export tool in graphics pane
+errors_plotly = ggplotly(errors_p, tooltip = c("x", "label", "y"))
+
+# Plot for ANOVA-like analysis
+p <- read_csv("../data/processed/subsystems.csv") %>%
+  select(-X1) %>%
+  mutate(subsystem = fct_relevel(subsystem, levels = c("Uptake", "EMP", "EDA", "PPP", "Pyr", "TCA"))) %>%
+  filter(author != "Ishii" & author != "Ec_core") %>%
+  ggplot(aes(x = subsystem, y = relative_error, color = subsystem))
+
+anova_p <- p + geom_jitter(height = 0, width = 0.1)
